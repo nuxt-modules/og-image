d250a9efe8f848d5c3677a2bc9abef8d4c57cf1d
Harlan Wilton <harlan@harlanzw.com>
2022-12-15 19:42:31 +1100

    fix(defineOgImage): config usage

--- src/runtime/composables/defineOgImage.ts
+++ src/runtime/composables/defineOgImage.ts
@@ -1,4 +1,5 @@
 import { useServerHead } from '@vueuse/head'
+import { useRouter } from '#imports'
 
 export const HtmlRendererRoute = '__og_image'
 export const RuntimeImageSuffix = 'og-image.png'
@@ -7,14 +8,14 @@ export const MetaOgImageContentPlaceholder = '__NUXT_OG_IMAGE_PLACEHOLDER__'
 export const LinkPrerenderId = 'nuxt-og-image-screenshot-path'
 
 export interface OgImagePayload {
-  runtime: boolean
-  title: string
-  description: string
-  component: string
+  runtime?: boolean
+  title?: string
+  description?: string
+  component?: string
   [key: string]: any
 }
 
-export function defineOgImage(options: OgImagePayload) {
+export function defineOgImage(options: OgImagePayload = {}) {
   if (process.server) {
     const router = useRouter()
     useServerHead({
@@ -24,13 +25,15 @@ export function defineOgImage(options: OgImagePayload) {
           content: () => options.runtime ? `${router.currentRoute.value.path}/${RuntimeImageSuffix}` : MetaOgImageContentPlaceholder,
         },
       ],
-      link: [
-        {
-          id: LinkPrerenderId,
-          rel: 'prerender',
-          href: `${router.currentRoute.value.path}/${HtmlRendererRoute}`,
-        },
-      ],
+      link: options.component
+        ? [
+            {
+              id: LinkPrerenderId,
+              rel: 'prerender',
+              href: `${router.currentRoute.value.path}/${HtmlRendererRoute}`,
+            },
+          ]
+        : [],
       script: [
         {
           id: PayloadScriptId,
