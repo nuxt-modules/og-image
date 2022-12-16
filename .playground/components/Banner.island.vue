a7ae3cf3e5f9ac4b366e823dd9445c3baf12b5fc
Harlan Wilton <harlan@harlanzw.com>
2022-12-16 22:53:55 +1100

    chore: add extra example

---
+++ .playground/components/Banner.island.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const props = defineProps({
+  // these will always be provided
+  path: String,
+  title: String,
+  description: String,
+  // anything custom comes here
+  backgroundImage: String
+})
+</script>
+
+<template>
+  <div class="wrap">
+    <div>
+      <h1>
+        {{ title }}
+      </h1>
+      <p>{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.wrap {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  background: linear-gradient(to bottom, #30e8bf, #ff8235);
+}
+
+h1 {
+  font-size: 4rem;
+}
+</style>
---
+++ .playground/pages/banner.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div>
+      Banner page
+    </div>
+    <OgImage component="Banner" title="Banner" description="My gtrgt" random-data="test" />
+  </div>
+</template>
