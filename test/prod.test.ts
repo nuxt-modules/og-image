0e7a987357449c82aea9bf37ee89db60269cae6c
Harlan Wilton <harlan@harlanzw.com>
2023-03-16 21:57:10 +1100

    test: add prod tests with image snapshots

---
+++ test/prod.test.ts
@@ -0,0 +1,43 @@
+import { describe, expect, it } from 'vitest'
+import { createResolver } from '@nuxt/kit'
+import { $fetch, setup } from '@nuxt/test-utils'
+import { toMatchImageSnapshot } from 'jest-image-snapshot'
+
+const { resolve } = createResolver(import.meta.url)
+
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toMatchImageSnapshot(): R
+    }
+  }
+}
+
+await setup({
+  rootDir: resolve('../.playground'),
+  build: true,
+  server: true,
+})
+
+expect.extend({ toMatchImageSnapshot })
+
+describe('build', () => {
+  it('basic', async () => {
+    async function fetchImage(url: string, options: any = {}) {
+      const blob: Blob = await $fetch(url, options)
+      return Buffer.from(await blob.arrayBuffer())
+    }
+
+    expect(await fetchImage('/__og_image__/og.png')).toMatchImageSnapshot()
+
+    expect(await fetchImage('/satori/image/__og_image__/og.png')).toMatchImageSnapshot()
+
+    expect(await fetchImage('/satori/with-options/__og_image__/og.png')).toMatchImageSnapshot()
+
+    expect(await fetchImage('/browser/component/__og_image__/og.png')).toMatchImageSnapshot()
+
+    expect(await fetchImage('/browser/custom/__og_image__/og.png')).toMatchImageSnapshot()
+
+    expect(await fetchImage('/satori/tailwind/__og_image__/og.png', { query: { title: 'Fully dynamic', bgColor: 'bg-green-500' } })).toMatchImageSnapshot()
+  }, 60000)
+})
