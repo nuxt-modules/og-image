08cd0c4f79df363d27f914421fca97c19ed8b897
Harlan Wilton <harlan@harlanzw.com>
2023-03-16 22:22:12 +1100

    fix: better flex inferences
    Fixes #23

--- src/runtime/nitro/renderers/satori/plugins/flex.ts
+++ src/runtime/nitro/renderers/satori/plugins/flex.ts
@@ -7,11 +7,12 @@ export default defineSatoriTransformer(() => {
     filter: (node: VNode) =>
       node.type === 'div'
       && (Array.isArray(node.props?.children) && node.props?.children.length >= 1)
-      && (!node.props.style?.display && !node.props?.class?.includes('flex')),
+      && (!node.props.style?.display && !node.props?.class?.includes('hidden')),
     transform: async (node: VNode) => {
       node.props.style = node.props.style || {}
       node.props.style.display = 'flex'
-      node.props.style.flexDirection = 'column'
+      if (!node.props?.class?.includes('flex-'))
+        node.props.style.flexDirection = 'column'
     },
   }
 })
