0f51801053797936298e856beda1203dc8caa0df
Harlan Wilton <harlan@harlanzw.com>
2023-04-29 01:07:40 +0700

    fix: maybe improve image loading for cloudflare

--- src/runtime/nitro/renderers/satori/plugins/imageSrc.ts
+++ src/runtime/nitro/renderers/satori/plugins/imageSrc.ts
@@ -1,6 +1,7 @@
 import { withBase } from 'ufo'
 import type { VNode } from '../../../../../types'
 import { defineSatoriTransformer } from '../utils'
+import { readPublicAssetBase64 } from '../../../utils'
 import { useNitroApp } from '#internal/nitro'
 
 // for relative links we embed them as base64 input or just fix the URL to be absolute
@@ -9,15 +10,41 @@ export default defineSatoriTransformer((url) => {
     filter: (node: VNode) => node.type === 'img',
     transform: async (node: VNode) => {
       const src = node.props?.src as string | null
-      const nitroApp = useNitroApp()
       if (src && src.startsWith('/')) {
-        // find the file using getAsset
-        const response = (await nitroApp.localFetch(src)) as Response
-        // see if we can fetch it from a kv host if we're using an edge provider
-        if (response.status === 200)
-          node.props.src = response.arrayBuffer()
-
-        else node.props.src = withBase(src, `${url.protocol}//${url.host}`)
+        let updated = false
+        const file = await readPublicAssetBase64(src)
+        if (file) {
+          node.props.src = file
+          updated = true
+        }
+        if (!updated) {
+          try {
+            const response = (await globalThis.$fetch(src)) as Response
+            // see if we can fetch it from a kv host if we're using an edge provider
+            node.props.src = response.arrayBuffer()
+            updated = true
+          }
+          catch (e) {
+          }
+        }
+        if (!updated) {
+          const nitroApp = useNitroApp()
+          // find the file using getAsset
+          try {
+            const response = (await nitroApp.localFetch(src)) as Response
+            // see if we can fetch it from a kv host if we're using an edge provider
+            if (response.status === 200) {
+              node.props.src = response.arrayBuffer()
+              updated = true
+            }
+          }
+          catch (e) {
+          }
+        }
+        if (!updated) {
+          // with query to avoid satori caching issue
+          node.props.src = `${withBase(src, `${url.protocol}//${url.host}`)}?${Date.now()}`
+        }
       }
     },
   }
