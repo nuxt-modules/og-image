4ea485b2c1e17db6df7bec60707b1a077002aa0d
Harlan Wilton <harlan@harlanzw.com>
2023-08-10 16:18:16 +0300

    fix: add cachedKeys to debug

--- src/runtime/nitro/routes/debug.ts
+++ src/runtime/nitro/routes/debug.ts
@@ -1,16 +1,21 @@
 import { defineEventHandler, setHeader } from 'h3'
-import { useRuntimeConfig, useSiteConfig } from '#imports'
+import { prefixStorage } from 'unstorage'
+import { useRuntimeConfig, useSiteConfig, useStorage } from '#imports'
 
 export default defineEventHandler(async (e) => {
   // set json header
   setHeader(e, 'Content-Type', 'application/json')
   const runtimeConfig = useRuntimeConfig()['nuxt-og-image']
   const siteConfig = await useSiteConfig(e)
+
+  const baseCacheKey = runtimeCacheStorage === 'default' ? `/cache/${module}@${version}` : `/${module}@${version}`
+  const cache = prefixStorage(useStorage(), `${baseCacheKey}/`)
   return {
     siteConfigUrl: {
       value: siteConfig.url,
       source: siteConfig._context.url || 'unknown',
     },
     runtimeConfig,
+    cachedKeys: await cache.keys(),
   }
 })
