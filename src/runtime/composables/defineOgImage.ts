7f6383593d81bfdf2b2f80f43b2a5786ffc62b83
Harlan Wilton <harlan@harlanzw.com>
2023-09-10 20:08:07 +0200

    fix: allow payload params to be processed

--- src/runtime/composables/defineOgImage.ts
+++ src/runtime/composables/defineOgImage.ts
@@ -83,6 +83,7 @@ export async function defineOgImage(_options: OgImageOptions = {}) {
         {
           id: 'nuxt-og-image-options',
           type: 'application/json',
+          processTemplateParams: true,
           innerHTML: () => {
             const payload = {
               title: '%s',
