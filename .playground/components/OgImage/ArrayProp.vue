b5e26636c815c41ea101719b97bb7707e263d97f
Harlan Wilton <harlan@harlanzw.com>
2023-10-03 20:42:27 +0200

    chore: add array prop example

---
+++ .playground/components/OgImage/ArrayProp.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps({
+  backgroundColor: String,
+  list: Array,
+});
+</script>
+<template>
+<div class="w-full h-full flex text-white items-center justify-center">
+  <h1 :style="{ color: 'red', fontSize: '70px' }">{{ list }}</h1>
+</div>
+</template>
---
+++ .playground/pages/satori/array-prop.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineOgImage({
+  component: 'ArrayProp',
+  list: [1, 2, 3],
+})
+</script>
+<template>
+  <div>
+    <div>
+      Default example with options
+    </div>
+  </div>
+</template>
