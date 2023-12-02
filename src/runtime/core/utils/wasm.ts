51c551d41f0d60be583af54103e8858440757ba1
Harlan Wilton <harlan@harlanzw.com>
2023-12-02 17:59:30 +1100

    chore: debug wasm

--- src/runtime/core/utils/wasm.ts
+++ src/runtime/core/utils/wasm.ts
@@ -1,9 +1,12 @@
 export async function importWasm(input: any) {
   const _input = await input
+  console.log('awaited wasm input', typeof _input, input)
   const _module = _input.default || _input
+  console.log('interop default', typeof _input, input)
   const _instance
     = typeof _module === 'function'
       ? await _module({}).then(r => r.instance || r)
       : await WebAssembly.instantiate(_module, {})
+  console.log('instance', typeof _instance, _instance, _instance.exports)
   return _instance.exports
 }
