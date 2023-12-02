d47adebbd9f32aa63cdffa2f61210dc93cc33ad7
Harlan Wilton <harlan@harlanzw.com>
2023-12-02 18:09:28 +1100

    chore: wrong debugging

--- src/runtime/core/utils/wasm.ts
+++ src/runtime/core/utils/wasm.ts
@@ -1,9 +1,10 @@
 export async function importWasm(input: any) {
+  console.log('pre', { type: typeof input, v: input })
   // may be a nested await for some reason
-  const _input = await (await input)
-  console.log('awaited wasm input', { type: typeof _input, v: input })
+  const _input = await input
+  console.log('awaited wasm input', { type: typeof _input, v: _input })
   const _module = _input.default || _input
-  console.log('interop default', { type: typeof _input, v: input })
+  console.log('interop default', { type: typeof _module, v: _module })
   const _instance
     = typeof _module === 'function'
       ? await _module({}).then(r => r.instance || r)
