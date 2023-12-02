fca1629c2b09800d947d2ca2f2e331622e9d9ad4
Harlan Wilton <harlan@harlanzw.com>
2023-12-02 18:47:53 +1100

    chore: remove debug

--- src/runtime/core/utils/wasm.ts
+++ src/runtime/core/utils/wasm.ts
@@ -1,21 +1,13 @@
 export async function importWasm(input: any) {
-  console.log('pre', { type: typeof input, v: input })
   // may be a nested await for some reason
   const _input = await input
-  console.log('awaited wasm input', { type: typeof _input, v: _input })
   const _module = _input.default || _input
-  console.log('interop default', { type: typeof _module, v: _module })
-  let _instance
   // this is from rollup/wasm, it does some magic we need to recover from
   if (typeof _module === 'function') {
-    console.log('interop rollup wasm fn')
     // empty input is to avoid instantiating the wasm module
     // this will just compile it
-    const fnRes = await _module().catch(e => {
-      console.error('interop module error', e)
-      return e
-    })
-    _instance = fnRes.instance || fnRes
+    const fnRes = await _module()
+    const _instance = fnRes.instance || fnRes
     return _instance.exports || _instance || _module
   }
   return _module
