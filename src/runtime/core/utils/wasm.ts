dcaed1ccf367f68e64caeadbb32b4b817716d378
Harlan Wilton <harlan@harlanzw.com>
2023-12-02 18:07:42 +1100

    chore: nested await

--- src/runtime/core/utils/wasm.ts
+++ src/runtime/core/utils/wasm.ts
@@ -1,8 +1,9 @@
 export async function importWasm(input: any) {
-  const _input = await input
-  console.log('awaited wasm input', typeof _input, input)
+  // may be a nested await for some reason
+  const _input = await (await input)
+  console.log('awaited wasm input', { type: typeof _input, v: input })
   const _module = _input.default || _input
-  console.log('interop default', typeof _input, input)
+  console.log('interop default', { type: typeof _input, v: input })
   const _instance
     = typeof _module === 'function'
       ? await _module({}).then(r => r.instance || r)
