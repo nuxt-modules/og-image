a0e0e305a63eba51abe57812108396be33c9a611
Harlan Wilton <harlan@harlanzw.com>
2023-12-05 01:00:25 +1100

    fix: allow empty props

--- src/runtime/composables/defineOgImageComponent.ts
+++ src/runtime/composables/defineOgImageComponent.ts
@@ -2,7 +2,7 @@ import type { ExtractComponentProps, OgImageOptions } from '../types'
 import { defineOgImage } from './defineOgImage'
 import type { OgImageComponents } from '#nuxt-og-image/components'
 
-export function defineOgImageComponent<T extends keyof OgImageComponents>(component: T, props: ExtractComponentProps<OgImageComponents[T]>, options: OgImageOptions = {}) {
+export function defineOgImageComponent<T extends keyof OgImageComponents>(component: T, props: Partial<ExtractComponentProps<OgImageComponents[T]>> = {}, options: OgImageOptions = {}) {
   return defineOgImage({
     ...options,
     component,
