2c5791806a9e3247dba226d620b84509452de8a0
Harlan Wilton <harlan@harlanzw.com>
2023-12-07 18:35:52 +1100

    fix: revert changes

--- src/runtime/core/cache/prerender.ts
+++ src/runtime/core/cache/prerender.ts
@@ -1,5 +1,6 @@
 import { createStorage } from 'unstorage'
 import lruCacheDriver from 'unstorage/drivers/lru-cache'
+import type { Browser } from 'playwright-core'
 import type { OgImageOptions } from '../../types'
 
 export const prerenderOptionsCache = import.meta.prerender
@@ -7,3 +8,5 @@ export const prerenderOptionsCache = import.meta.prerender
     driver: lruCacheDriver({ max: 1000 }),
   })
   : undefined
+
+export const prerenderChromiumContext: { browser?: Browser } = { browser: undefined }
