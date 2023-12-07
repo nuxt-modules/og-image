a2a3a024be1872ebb023032666bb11b3ff1a2617
Harlan Wilton <harlan@harlanzw.com>
2023-12-07 18:53:43 +1100

    fix: close browser when prerendering

--- src/runtime/core/renderers/chromium/index.ts
+++ src/runtime/core/renderers/chromium/index.ts
@@ -1,4 +1,5 @@
 import type { Browser } from 'playwright-core'
+import { createError } from 'h3'
 import type { Renderer } from '../../../types'
 import { createScreenshot } from './screenshot'
 import { createBrowser } from '#nuxt-og-image/bindings/chromium'
@@ -11,22 +12,15 @@ const ChromiumRenderer: Renderer = {
   },
   async createImage(ctx) {
     const browser: Browser = await createBrowser()
-    // check if browser is open
-    if (!browser.isConnected()) {
-      return createError({
-        statusCode: 400,
-        statusMessage: 'Failed to create connect to Chromium Browser.',
-      })
-    }
     const screenshot = await createScreenshot(ctx, browser!)
       .catch(e => e)
+    await browser.close()
     if (screenshot instanceof Error) {
       return createError({
         statusCode: 400,
         statusMessage: `Failed to create screenshot ${screenshot.message}.`,
       })
     }
-    await browser.close()
     return screenshot
   },
 }
