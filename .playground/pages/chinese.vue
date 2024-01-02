8cbffeaa73b9ff75058ae0fdff653d3d0b949e27
Harlan Wilton <harlan@harlanzw.com>
2024-01-02 15:09:17 +1100

    chore: add chinese example

---
+++ .playground/pages/chinese.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { defineOgImageComponent } from '#imports'
+
+defineOgImageComponent('NuxtSeo', {
+  title: '中文測試中文測試中文測試中文測試中文測試中文測試中文測試中文測試',
+})
+</script>
+
+<template>
+  <span lang="zh-CN">chinese</span>
+</template>
