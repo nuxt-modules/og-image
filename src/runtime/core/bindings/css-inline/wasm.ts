92abe99a865e2b7d076ca7a772bf752ee2bd1e6a
Harlan Wilton <harlan@harlanzw.com>
2024-01-03 23:40:44 +1100

    fix: wrong css-inline wasm path

--- src/runtime/core/bindings/css-inline/wasm.ts
+++ src/runtime/core/bindings/css-inline/wasm.ts
@@ -2,7 +2,7 @@ import { initWasm, inline } from '@css-inline/css-inline-wasm'
 import { importWasm } from '../../utils/wasm'
 
 export default {
-  initWasmPromise: initWasm(importWasm(import('@resvg/resvg-wasm/index_bg.wasm' as string))),
+  initWasmPromise: initWasm(importWasm(import('@css-inline/css-inline-wasm/index_bg.wasm' as string))),
   cssInline: {
     inline,
   },
