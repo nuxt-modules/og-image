8d1321a435c99486e60a1a9d2bd9f0215a42eeea
Harlan Wilton <harlan@harlanzw.com>
2024-01-04 00:26:26 +1100

    fix: css-inlining logic for new island css processing

--- src/runtime/core/html/applyInlineCss.ts
+++ src/runtime/core/html/applyInlineCss.ts
@@ -6,36 +6,43 @@ import cssInline from '#nuxt-og-image/bindings/css-inline'
 
 export async function applyInlineCss({ e }: OgImageRenderEventContext, island: NuxtIslandResponse) {
   let html = island.html
-  const componentInlineStyles = island.head.link.filter(l => l.href.startsWith('/_nuxt/components'))
-  const usingInlineCss = !!componentInlineStyles.length
   // inline styles from the island
   // empty.mjs returns an __unenv__ object as true
-  if (!cssInline.__unenv__ && componentInlineStyles.length) {
-    const css = island.head.style.map(s => s.innerHTML).join('\n')
-    const linksToCss = (await Promise.all(
-      componentInlineStyles
-        .map((l) => {
-          return e.$fetch<string>(`${l.href}&hmr=false`, {
-            responseType: 'text',
-            baseURL: useNitroOrigin(e),
-          }).then((res) => {
-            return res.trim().split('\n').filter(l => !l.startsWith('//')).join('\n').trim()
-          })
-        }),
-    )).join('\n')
-    const cssToInline = `${linksToCss}${css}`
+  if (!cssInline.__unenv__) {
+    let css = island.head.style.map(s => s.innerHTML).join('\n')
+    const componentInlineStyles = island.head.link.filter(l => l.href.startsWith('/_nuxt/components'))
+    if (process.dev) {
+      const linksToCss = componentInlineStyles.length
+        ? (await Promise.all(
+            componentInlineStyles
+              .map((l) => {
+                return e.$fetch<string>(`${l.href}&hmr=false`, {
+                  responseType: 'text',
+                  baseURL: useNitroOrigin(e),
+                }).then((res) => {
+                  return res.trim().split('\n').filter(l => !l.startsWith('//')).join('\n').trim()
+                })
+              }),
+          )).join('\n')
+        : ''
+      css = `${linksToCss}${css}`
+    }
+    // avoid loading css-inline wasm if we don't need
+    if (!css.trim().length)
+      return false
     const cssInline = await useCssInline()
     html = cssInline.inline(island.html, {
       loadRemoteStylesheets: false,
-      extraCss: cssToInline,
+      extraCss: css,
     })
     // extract classses from the css
-    const classes = cssToInline.match(/\.([a-zA-Z0-9-_]+)/g)?.map(c => c.replace('.', ''))
+    const classes = css.match(/\.([a-zA-Z0-9-_]+)/g)?.map(c => c.replace('.', ''))
     // remove classes from the html to avoid satori errors
     if (classes)
       html = html.replace(new RegExp(`class="(${classes.join('|')})"`, 'g'), '')
 
     island.html = html
+    return true
   }
-  return usingInlineCss
+  return false
 }
