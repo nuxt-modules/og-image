039f429f2bf48a45795f03d542a92c06577128c1
harlan <harlan@harlanzw.com>
2024-03-16 11:28:36 +1100

    chore: bump deps and lint

--- src/runtime/nitro/og-image/bindings/css-inline/wasm.ts
+++ src/runtime/nitro/og-image/bindings/css-inline/wasm.ts
@@ -1,7 +1,7 @@
 import { initWasm, inline } from '@css-inline/css-inline-wasm'
 
 export default {
-  initWasmPromise: initWasm(import('@css-inline/css-inline-wasm/index_bg.wasm?module' as string).then((r) => r.default || r)),
+  initWasmPromise: initWasm(import('@css-inline/css-inline-wasm/index_bg.wasm?module' as string).then(r => r.default || r)),
   cssInline: {
     inline,
   },
