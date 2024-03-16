7a99f44fcf7ba12d1642c4e254f62f36ba2e3227
harlan <harlan@harlanzw.com>
2024-03-16 11:25:26 +1100

    fix: interop default wasm exports

--- src/runtime/nitro/og-image/bindings/resvg/wasm.ts
+++ src/runtime/nitro/og-image/bindings/resvg/wasm.ts
@@ -1,6 +1,6 @@
 import { Resvg as _Resvg, initWasm } from '@resvg/resvg-wasm'
 
 export default {
-  initWasmPromise: initWasm(import('@resvg/resvg-wasm/index_bg.wasm?module' as string)),
+  initWasmPromise: initWasm(import('@resvg/resvg-wasm/index_bg.wasm?module' as string).then((r) => r.default || r)),
   Resvg: _Resvg,
 }
--- src/runtime/nitro/og-image/bindings/satori/wasm.ts
+++ src/runtime/nitro/og-image/bindings/satori/wasm.ts
@@ -3,7 +3,7 @@ import initYoga from 'yoga-wasm-web'
 import { init } from 'satori'
 
 const wasm = import('yoga-wasm-web/dist/yoga.wasm?module' as string)
-  .then(async yoga => await initYoga(yoga))
+  .then(async yoga => await initYoga(yoga.default || yoga))
 
 export default {
   initWasmPromise: new Promise<void>((resolve) => {
