0bfc8c669fb768a70946b5294b132b4561d4dcb8
harlan <harlan@harlanzw.com>
2024-07-11 18:08:56 +1000

    fix: saner default cache base path

--- src/module.ts
+++ src/module.ts
@@ -497,7 +497,7 @@ declare module '#nuxt-og-image/unocss-config' {
 
     const cacheEnabled = typeof config.runtimeCacheStorage !== 'undefined' && config.runtimeCacheStorage !== false
     const runtimeCacheStorage = typeof config.runtimeCacheStorage === 'boolean' ? 'default' : config.runtimeCacheStorage.driver
-    let baseCacheKey: string | false = runtimeCacheStorage === 'default' ? `/cache/nuxt-og-image@${version}` : `/nuxt-og-image/${version}`
+    let baseCacheKey: string | false = runtimeCacheStorage === 'default' ? `/cache/nuxt-og-image/${version}` : `/nuxt-og-image/${version}`
     if (!cacheEnabled)
       baseCacheKey = false
     if (!nuxt.options.dev && config.runtimeCacheStorage && typeof config.runtimeCacheStorage === 'object') {
