00d09e8d70b12f3405ab2836a2b89b1c0826bc81
harlan <harlan@harlanzw.com>
2024-07-26 13:53:40 +1000

    fix: support `SERVER_PRESET` to detect env

--- src/compatibility.ts
+++ src/compatibility.ts
@@ -117,7 +117,7 @@ export function resolveNitroPreset(nitroConfig?: NitroConfig): string {
   if (nitroConfig && nitroConfig?.preset)
     preset = nitroConfig.preset
   if (!preset)
-    preset = env.NITRO_PRESET || detectTarget() || 'node-server'
+    preset = env.NITRO_PRESET || env.SERVER_PRESET || detectTarget() || 'node-server'
   return preset.replace('_', '-') // sometimes they are different
 }
 
