5096d93a2fd060c1635a688c7e0544c715de23c3
harlan <harlan@harlanzw.com>
2024-09-11 14:57:34 +1000

    chore: using alias

--- src/runtime/nitro/og-image/cache/mock.ts
+++ src/runtime/nitro/og-image/cache/mock.ts
@@ -1,7 +1,7 @@
 import { createStorage } from 'unstorage'
 import memoryDriver from 'unstorage/drivers/memory'
 import type { Storage } from 'unstorage'
-import type { OgImageOptions } from '~/src/runtime/types'
+import type { OgImageOptions } from '../../../types'
 
 const storage = createStorage<OgImageOptions>({
   driver: memoryDriver(),
