24d4d2592d4144b38fffe28c8b5709e5c548c37f
harlan <harlan@harlanzw.com>
2024-09-12 00:55:25 +1000

    chore: misc

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -3,12 +3,15 @@ import { resolve } from 'pathe'
 
 export default defineNuxtConfig({
   ssr: false,
+
   modules: [
     DevtoolsUIKit,
   ],
+
   devtools: {
     enabled: false,
   },
+
   nitro: {
     prerender: {
       routes: [
@@ -19,10 +22,14 @@ export default defineNuxtConfig({
       publicDir: resolve(__dirname, '../dist/client'),
     },
   },
+
   app: {
     baseURL: '/__nuxt-og-image',
   },
+
   experimental: {
     componentIslands: true,
   },
+
+  compatibilityDate: '2024-09-11',
 })
