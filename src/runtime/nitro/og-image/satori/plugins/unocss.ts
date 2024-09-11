a74426c86d6d34e1cb451a2267eb8388d1a15297
harlan <harlan@harlanzw.com>
2024-09-12 03:56:33 +1000

    fix: support fallback gray colors

--- src/runtime/nitro/og-image/satori/plugins/unocss.ts
+++ src/runtime/nitro/og-image/satori/plugins/unocss.ts
@@ -23,7 +23,19 @@ export default defineSatoriTransformer({
       const parsedToken = await uno.parseToken(token)
       if (parsedToken) {
         const inlineStyles = parsedToken[0][2].split(';').filter(s => !!s?.trim())
-        const vars: Record<string, string> = {}
+        const vars: Record<string, string> = {
+          '--color-gray-50': '249 250 251',
+          '--color-gray-100': '243 244 246',
+          '--color-gray-200': '229 231 235',
+          '--color-gray-300': '209 213 219',
+          '--color-gray-400': '156 163 175',
+          '--color-gray-500': '107 114 128',
+          '--color-gray-600': '75 85 99',
+          '--color-gray-700': '55 65 81',
+          '--color-gray-800': '31 41 55',
+          '--color-gray-900': '17 24 39',
+          '--color-gray-950': '3 7 18',
+        }
         inlineStyles.filter(s => s.startsWith('--'))
           .forEach((s) => {
             const [key, value] = s.split(':')
@@ -43,7 +55,7 @@ export default defineSatoriTransformer({
             if (styles[camelCasedKey] && styles[camelCasedKey].includes('/')) {
               const [rgb, opacity] = styles[camelCasedKey].split('/')
               if (opacity.trim() === '1)')
-                styles[camelCasedKey] = rgb.replace(/(\d+) (\d+) (\d+).*/, (_, r, g, b) => `${r}, ${g}, ${b})`)
+                styles[camelCasedKey] = rgb.replace(/(\d+) (\d+) (\d).*/, (_, r, g, b) => `${r}, ${g}, ${b})`)
               else
                 styles[camelCasedKey] = `${rgb.replace('rgb', 'rgba').replaceAll(' ', ', ')}${opacity.trim()}`
             }
