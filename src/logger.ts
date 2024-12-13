3370dca183f0e82ee9ec6ccf9227eec02c53b16c
Harlan Wilton <harlan@harlanzw.com>
2024-12-13 16:36:34 +1100

    fix: throw error when using `defineOgImage*` fns client-side (#293)

--- src/logger.ts
+++
@@ -1,6 +0,0 @@
-import { useLogger } from '@nuxt/kit'
-import { colorize } from 'consola/utils'
-
-export const logger = useLogger('@nuxtjs/og-image')
-
-export const gray = (s: string) => colorize('gray', s)
