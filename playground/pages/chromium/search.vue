220332eb615189920b70c3aa6c458096d91eb67e
harlan <harlan@harlanzw.com>
2024-12-15 14:34:03 +1100

    chore: improve search playground

---
+++ playground/pages/chromium/search.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="px-7 my-5">
+    <OgImageScreenshot />
+  </div>
+</template>
--- playground/pages/search.vue
+++ playground/pages/search.vue
@@ -1,5 +1,14 @@
+<script lang="ts" setup>
+import { defineOgImageComponent, useRoute } from '#imports'
+
+const route = useRoute()
+defineOgImageComponent('NuxtSeo', {
+  title: String(route.query?.q) || 'no title',
+})
+</script>
+
 <template>
   <div class="px-7 my-5">
-    <OgImageScreenshot />
+    search: {{ route.query?.q }}
   </div>
 </template>
