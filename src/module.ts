b048347b07c8bc7508e85fcb7054e97bc4699cf1
harlan <harlan@harlanzw.com>
2025-03-08 12:03:33 +1100

    fix!: require Nuxt v3.16

--- src/module.ts
+++ src/module.ts
@@ -144,7 +144,7 @@ export default defineNuxtModule<ModuleOptions>({
   meta: {
     name: 'nuxt-og-image',
     compatibility: {
-      nuxt: '>=3.10.3',
+      nuxt: '>=3.16.0',
       bridge: false,
     },
     configKey: 'ogImage',
