6af5ddb7c8003b398d0d4f2b4131bb86d91ad8f5
Harlan Wilton <harlan@harlanzw.com>
2025-06-19 12:27:29 +1000

    chore: fix test setup

---
+++ vitest.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig, defineProject } from 'vitest/config'
+import { defineVitestProject } from '@nuxt/test-utils/config'
+
+export default defineConfig({
+  test: {
+    projects: [
+      // utils folders as *.test.ts in either test/unit or in src/**/*.test.ts
+      defineProject({
+        test: {
+          name: 'unit',
+          environment: 'node',
+          include: [
+            './**/*.test.ts',
+          ],
+          exclude: [
+            './test/e2e/**/*.test.ts',
+            '**/node_modules/**',
+          ],
+        },
+      }),
+      // e2e tests in test/e2e
+      defineVitestProject({
+        test: {
+          name: 'e2e',
+          include: [
+            './test/e2e/**/*.test.ts',
+          ],
+          exclude: [
+            '**/node_modules/**',
+          ],
+        },
+      }),
+    ],
+  },
+})
