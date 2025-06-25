76b7c299cfa48b05fef409af900b71479dcff678
Harlan Wilton <harlan@harlanzw.com>
2025-06-25 20:47:10 +1000

    fix(content): type broken when augmenting
    Fixes https://github.com/harlan-zw/nuxt-seo/issues/450

--- src/content.ts
+++ src/content.ts
@@ -2,20 +2,26 @@ import type { Collection } from '@nuxt/content'
 import type { TypeOf, ZodRawShape } from 'zod'
 import { z } from '@nuxt/content'
 
+export const ogImageSchema = z.object({
+  url: z.string().optional(),
+  component: z.string().optional(),
+  props: z.record(z.string(), z.any()),
+}).optional()
+
 export const schema = z.object({
-  ogImage: z.object({
-    url: z.string().optional(),
-    component: z.string().optional(),
-    props: z.record(z.string(), z.any()),
-  }).optional(),
+  ogImage: ogImageSchema,
 })
 
 export type OgImageSchema = TypeOf<typeof schema>
 
-export function asOgImageCollection<T extends ZodRawShape>(collection: Collection<T>): Collection<T> {
+type ExtendedSchema<T extends ZodRawShape> = T & {
+  ogImage: typeof ogImageSchema
+}
+
+export function asOgImageCollection<T extends ZodRawShape>(collection: Collection<T>): Collection<ExtendedSchema<T>> {
   if (collection.type === 'page') {
     // @ts-expect-error untyped
     collection.schema = collection.schema ? schema.extend(collection.schema.shape) : schema
   }
-  return collection
+  return collection as Collection<ExtendedSchema<T>>
 }
