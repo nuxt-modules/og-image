053c5e89d7ab92b7266a7599d275b6e19c3a6da0
Thibaud Ollagnier <ThibaudDauce@users.noreply.github.com>
2026-03-10 02:58:18 +0100

    fix: replacing .png inside the component params (#493)

---
+++ test/fixtures/basic/components/OgImage/PropTest.satori.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+  logo?: string
+}>(), {
+  logo: '',
+})
+</script>
+
+<template>
+  <div class="w-full h-full flex items-center justify-center bg-white">
+    <p class="text-2xl">
+      {{ logo }}
+    </p>
+  </div>
+</template>
