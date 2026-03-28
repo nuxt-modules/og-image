fdbf7413b97f708ad58e9b482464891d9e73cbbc
Harlan Wilton <harlan@harlanzw.com>
2026-03-28 16:44:37 +1100

    fix: remove unused import

--- test/unit/url-signing.test.ts
+++ test/unit/url-signing.test.ts
@@ -1,7 +1,6 @@
 import { describe, expect, it } from 'vitest'
 import {
   buildOgImageUrl,
-  encodeOgImageParams,
   signEncodedParams,
   verifyOgImageSignature,
 } from '../../src/runtime/shared/urlEncoding'
